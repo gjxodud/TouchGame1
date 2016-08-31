120eac6692b894b41a9ff277322a62789e6e07f0
aorhqq <aorhqq@gmail.com>
2016-08-31 21:07:46 +0900

    메인엑티비티

---
+++ app/src/main/java/com/example/user/touchgame/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.user.touchgame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.Touch;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void five(View v){
+        Intent intent = new Intent(getApplicationContext(), TouchGame.class);
+        intent.putExtra("second",5);
+        startActivity(intent);
+    }
+
+    public void ten(View v){
+        Intent intent = new Intent(getApplicationContext(), TouchGame.class);
+        intent.putExtra("second",5);
+        startActivity(intent);
+    }
+}
